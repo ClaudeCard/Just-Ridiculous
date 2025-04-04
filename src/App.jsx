6b2c8cc32ac8d0a0338e1ba0bbb9422a5ff9a05b
ClaudeCard <claudecard710@gmail.com>
2025-04-04 02:10:10 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-import React from 'react'
+import React from "react";
+import AetheronOrb from "./components/AetheronOrb";
 
-function App() {
+export default function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gradient-to-br from-indigo-800 to-purple-900 text-white">
-      <h1 className="text-4xl font-bold">Welcome to the Ridiculous Universe</h1>
-    </div>
-     <div className="min-h-screen bg-black text-white">
-      {/* Other content */}
+    <div className="flex items-center justify-center min-h-screen bg-black">
       <AetheronOrb />
     </div>
-  )
+  );
 }
-
-export default App
